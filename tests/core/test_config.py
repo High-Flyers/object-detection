350a8f8e312518718f99c0a91e087701d4ea0edd
Karolina Kozubik <k4aro0@gmail.com>
2023-11-08 20:46:51 +0100

    correct config tests

--- tests/core/test_config.py
+++ tests/core/test_config.py
@@ -4,10 +4,11 @@ from pathlib import Path
 from object_detection.core.config import Config
 
 TEST_SETTINGS_PATH = Path("tests/core/settings.test.yaml")
+EMPTY_SETTINGS_PATH = Path("test/core/empty-settings.test.yaml")
 
 
 def test_default_variables():
-    config = Config()
+    config = Config(config_path=EMPTY_SETTINGS_PATH)
     assert config.dataset_file_format == "png"
     assert config.file_count_threshold == 20
     assert config.dataset_split == 0.2
